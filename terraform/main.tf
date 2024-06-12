de0a8057e3507c862a596229dec4eedcdbfc1475
shimigui <miguel.nasto@gmail.com>
2024-06-12 11:52:32 -0300

    feat: adding module 'vpc' in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Provider configurations
 provider "aws" {
-  region = "us-east-1"
+  region = "sa-east-1"
 }
 
 data "aws_ami" "ubuntu" {
@@ -35,6 +35,25 @@ resource "aws_instance" "web" {
 
   tags = {
     Name = "ANiceName"
+  }
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = aws_instance.web.key_name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["sa-east-1a", "sa-east-1b", "sa-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
