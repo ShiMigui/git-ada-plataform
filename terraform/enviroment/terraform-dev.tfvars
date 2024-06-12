5eb6129c852071776a3e471718dcd833d8864286
shimigui <miguel.nasto@gmail.com>
2024-06-12 12:21:31 -0300

    feat: add terraform variables

---
+++ terraform/enviroment/terraform-dev.tfvars
@@ -0,0 +1,3 @@
+aws_region    = "sa-east-1"
+vpc_name      = "terraform-introduction-vpc"
+resource_name = "terraform-introduction-instance"
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "resource_name" {
+  type = string
+}
